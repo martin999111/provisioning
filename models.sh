fcf3d9a495fba4ed6d0280f146b99812c893fb27
martin999111 <martin.st.work@gmail.com>
2025-06-02 01:02:44 +0200

    Update models.sh

--- models.sh
+++ models.sh
@@ -6,3 +6,5 @@
 
 wget https://fsn1.your-objectstorage.com/sf-models/checkpoints/bigLust_v16.safetensors
 wget https://fsn1.your-objectstorage.com/sf-models/checkpoints/realismEngineSDXL_v30VAE.safetensors
+wget https://fsn1.your-objectstorage.com/sf-models/clip_vision/CLIP-ViT-H-14-laion2B-s32B-b79K.safetensors
+wget https://fsn1.your-objectstorage.com/sf-models/clip_vision/CLIP-ViT-bigG-14-laion2B-39B-b160k.safetensors
