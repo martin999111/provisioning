73c65b7ed31dd4b6dd13631085dbe3da777b44f4
martin999111 <martin.st.work@gmail.com>
2025-06-02 00:46:55 +0200

    Create models.sh

---
+++ models.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://fsn1.your-objectstorage.com/sf-models/checkpoints/bigLust_v16.safetensors
+wget https://fsn1.your-objectstorage.com/sf-models/checkpoints/realismEngineSDXL_v30VAE.safetensors
