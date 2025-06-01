42b99700620456605eabd481b978bc0e4bca6742
martin999111 <martin.st.work@gmail.com>
2025-06-02 00:58:38 +0200

    Update models.sh

--- models.sh
+++ models.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#1. RM Comfy folder
+#2. DL backup
+#3. PIP install latest from backup /.venv-backups/{source-instance-id}/
+#pip install --no-cache-dir -r venv-main-latest.txt workspace/
 
 wget https://fsn1.your-objectstorage.com/sf-models/checkpoints/bigLust_v16.safetensors
 wget https://fsn1.your-objectstorage.com/sf-models/checkpoints/realismEngineSDXL_v30VAE.safetensors
